42427e1dff3625423d85f73c78a18a92e7e29063
Mandi Coder <mandicoder@proton.me>
2024-01-10 01:28:35 +0000

    gitpod

--- iniciar_gitpod.py
+++ iniciar_gitpod.py
@@ -1,4 +1,5 @@
 from subprocess import Popen
+import subprocess
 from os import environ
 
 environ["BOT_TOKEN"] = '6953816594:AAG-ECps_eDr4a38_xmTUZmrM_ONIRbNHh4'
@@ -9,8 +10,6 @@ environ["BOT_USER"] = 'MandiCoder'
 environ["API_HASH"] = 'ff9d2b13d574fd0206a14bd3ceac7502'
 environ["API_ID"] = '23053083'
 
-comando_version = ['export', 'PYENV_VERSION=3.10']
 comando_iniciar = ['python3', 'main.py']
 
-Popen(comando_version)
 Popen(comando_iniciar)
