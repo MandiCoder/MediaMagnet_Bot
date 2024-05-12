07d7ed3ef89ef359ca93e138a5322881187efad7
Mandi Coder <mandicoder@proton.me>
2024-05-12 14:51:42 +0000

    download playlist

--- tests/download_playlist.py
+++ tests/download_playlist.py
@@ -9,8 +9,10 @@ def descargar_playlist(url):
     for video in playlist.videos:
         print(f"Descargando: {video.title}\nRestantes: {num_videos}")
         num_videos -= 1
-        video.streams.get_highest_resolution().download(output_path="downloads/MandiCoder")
+        try:
+            video.streams.get_highest_resolution().download(output_path="downloads/MandiCoder")
+        except:
+            print("Error")
 
 # Reemplaza 'url_de_tu_playlist' con la URL de tu lista de reproducción de YouTube
-descargar_playlist("https://youtube.com/playlist?list=PLU8oAlHdN5Blq85GIxtKjIXdfHPksV_Hm&si=8eLm0w8yT-OBOyad")
-1
+descargar_playlist("https://youtube.com/playlist?list=PL0gZ8Ba6dZ7KqKGpl97skDQ06hmk3vq7L&si=QqfaqbQdafcyQ_2k")
